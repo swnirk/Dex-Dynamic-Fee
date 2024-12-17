dd89bbebc0b186a39fd092a03bc4960e021b4b6d
Dmitrii Umnov <umnovdimitry@icloud.com>
2024-12-17 23:34:44 +0100

    Pass more data to SimulationResult; Plot fee algorithm price EMA vs real price

--- visualizations/utility.py
+++ visualizations/utility.py
@@ -1,5 +1,7 @@
 import sys
 import os
+from matplotlib.ticker import MaxNLocator
+from matplotlib.pyplot import Axes
 
 
 # Add the project root to the path so that we can import the modules
@@ -15,3 +17,9 @@ def get_initial_pool_sizes(price_A: float, price_B: float, total_pool: float) ->
     q_A = total_pool / (r + 1)
     q_B = total_pool - q_A
     return q_A, q_B
+
+
+def fix_x_axis_labels(ax: Axes):
+    ax.xaxis.set_major_locator(
+        MaxNLocator(nbins=10)
+    )  # Set to show a maximum of 10 ticks
